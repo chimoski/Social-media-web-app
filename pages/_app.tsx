56f031e51984d4d8911b024aefef23b83b2972b1
chimoski <chimaobiprince867@gmail.com>
2022-07-04 13:50:06 +0100

    first-commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {useState, useEffect} from 'react'
+import Navbar from '../components/Navbar';
+import SideBar from '../components/SideBar';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp= ({ Component, pageProps }: AppProps)=> {
+
+  const [isSSr, setIsSSr] = useState(true);
+  useEffect(()=>{
+    setIsSSr(false);
+  });
+
+  if(isSSr) return null
+
+  return(
+    <>
+    <Navbar/>
+    <div className='flex gap-6 md:gap-20'>
+    <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+      <SideBar />
+    </div>
+    <div className='mt-4 flex flex-col gaap-10 overflow-auto h-[88vh] video flex-1'>
+      <Component {...pageProps} />
+    </div>
+    </div>
+     
+    </>
+     )
 }
 
 export default MyApp
